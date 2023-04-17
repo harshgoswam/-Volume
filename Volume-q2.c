7aa6b42c2ef6ef9e84ec0d74bc60c860765bf6c4
harshgoswam <122149827+harshgoswam@users.noreply.github.com>
2023-04-17 12:39:11 +0530

    Update Volume-q2.c

--- Volume-q2.c
+++ Volume-q2.c
@@ -4,7 +4,7 @@ struct Marks
 {
     int  roll_no;
     char  name [100];
-    float  chem_marks, maths_marks , phy_marks;
+    int  chem_marks, maths_marks , phy_marks;
     
 };
 
@@ -24,16 +24,19 @@ int main()
         printf("Enter name :\n");
         scanf("%s",marks[i].name);
         printf("Enter Chemistry marks :\n");
-        scanf("%f", &marks[i].chem_marks);
+        scanf("%d", &marks[i].chem_marks);
         printf("Enter Maths marks :\n");
-        scanf("%f", &marks[i].maths_marks);
+        scanf("%d", &marks[i].maths_marks);
         printf("Enter Physics marks :\n");
-        scanf("%f", &marks[i].phy_marks);
+        scanf("%d", &marks[i].phy_marks);
     }
     for (i=0;i<n;i++)
     {
         printf("Student %d\n",i+1);
 	    percentage = (marks[i].chem_marks + marks[i].maths_marks + marks[i].phy_marks)*100/300;
-	    printf("Percentage : %0.2f\n", percentage);
-    }
+	    
+	   	printf("\nName=%s\nRoll-no=%d\nChemestry Marks=%d\nMaths Marks=%d\nPhysics Marks=%d\n",
+		marks[i].name,marks[i].roll_no,marks[i].chem_marks,marks[i].maths_marks,marks[i].phy_marks);
+
+	    printf("Percentage : %0.2f\n", percentage);	}
 }
